5f1e65a3632c12ab14f7552c90bed1fd9cb9913f
Chen Yufei <cyfdecyf@gmail.com>
2010-07-29 23:30:52 -0400

    Do not link with libtopology for util programs.

--- utils/module.mk
+++ utils/module.mk
@@ -2,16 +2,14 @@ bindir    := $(BINDIR)/utils
 #programs  += $(bindir)/kill $(bindir)/monitor
 programs  += $(bindir)/watch-test $(bindir)/profile-tool $(bindir)/monitor
 
-UTILS_LINK := -lcurses
-
 $(bindir)/watch-test:  $(OBJDIR)/utils/watch-test.o
 	@mkdir -p $(@D)
-	$(call quiet-command, $(LD) $(UTILS_LINK) -o $@ $^, "  LINK    $@")
+	$(call quiet-command, $(LD) -o $@ $^, "  LINK    $@")
 
 $(bindir)/profile-tool: $(OBJDIR)/utils/profile-tool.o
 	@mkdir -p $(@D)
-	$(call quiet-command, $(LD) $(LDFLAGS) $(UTILS_LINK) -o $@ $^, "  LINK    $@")
+	$(call quiet-command, $(LD) -o $@ $^, "  LINK    $@")
 
 $(bindir)/monitor:  $(OBJDIR)/utils/monitor.o
 	@mkdir -p $(@D)
-	$(call quiet-command, $(LD) $(LDFLAGS) $(UTILS_LINK) -o $@ $^, "  LINK    $@")
+	$(call quiet-command, $(LD) -lcurses -o $@ $^, "  LINK    $@")
